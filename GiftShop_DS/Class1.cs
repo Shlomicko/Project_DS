b70d1c8a31180ebb78cc6e79b257ada15ed3330b
Shlomicko <shlomicko@gmail.com>
2018-05-01 15:52:33 +0300

    Add project files.

---
+++ GiftShop_DS/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiftShop_DS
+{
+    public class Class1
+    {
+    }
+}
